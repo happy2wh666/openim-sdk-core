077efaa7149cce6f2c13f4afaf6f27409e479e09
Your Name <you@example.com>
2025-07-29 22:11:16 +0800

    清空某些本地数据库表，比如群成员、群信息、好友等等

--- pkg/db/db_init.go
+++ pkg/db/db_init.go
@@ -177,6 +177,7 @@ func (d *DataBase) ClearSomeLocalCache(ctx context.Context) error {
 	d.mRWMutex.Lock()
 	defer d.mRWMutex.Unlock()
 	// 使用 Delete 方法清空整个表（不带 Where 条件）
+	log.ZDebug(ctx, "will ClearSomeLocalCache on next step")
 	return d.conn.WithContext(ctx).Session(&gorm.Session{AllowGlobalUpdate: true}).
 		Delete(
 			&model_struct.LocalFriend{},
